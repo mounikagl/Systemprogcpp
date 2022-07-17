6d55532d0a92f52b85bd7a8260804a215812f149
mounikagl <108278761+mounikagl@users.noreply.github.com>
2022-07-17 20:34:32 +0530

    Add files via upload

---
+++ assgnment/Multiprocessing_assignments/inc/multiassgn.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stdio.h>
+#define PATH "/home/mounika/sysprg/Multiprocessing_assignments/data"
+
+using namespace std;
